7f1bf713b4957f5560867447c754ae7589078b65
Balgrat <jeroen.jerry@hotmail.com>
2024-05-11 18:37:27 +0200

    Update

---
+++ src/ShoppingList/README.md
@@ -0,0 +1,29 @@
+# Shopping List
+
+A small project to login and keep track of a shopping list
+
+
+## TODO's
+
+The idea for this is to use several new technologies (new for me) to make this simple project which I might even use?
+
+My initial approach would be:
+
+- Login to your account
+- Show all shopping lists of this user
+- Allow multiple users per shopping list (shopping list is main db entry)
+- Don't worry about simultaneous editting, but would be nice if it worked
+- Use mongoDB as database
+- Front-end with React
+
+
+
+## Expansions
+
+- Taking notes
+- Cross compatible on Android? iOS? lol no iOS most likely
+
+
+
+###### If anyone finds this, pls no flame :( just experimenting
+###### Constructive feedback always welcome of course :)
---
+++ src/ShoppingList/mongodb.js
@@ -0,0 +1,28 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// TODO make password a secret variable on repo and put in here (properly encoded though)
+const password = encodeURIComponent("<password>");
+const uri = `mongodb+srv://Balgrat:${password}@balgratdb.nutmpdr.mongodb.net/?retryWrites=true&w=majority&appName=BalgratDB`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
