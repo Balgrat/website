bf9d75d93717c5ebbd36a046c5821303cd0ebaf8
Balgrat <jeroen.jerry@hotmail.com>
2024-05-12 13:13:00 +0200

    Move everything to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Shopping from './pages/Shopping';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path="shopping" element={<Shopping/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 root.render(
   <React.StrictMode>
     <App />
